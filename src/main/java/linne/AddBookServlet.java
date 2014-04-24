ab0ea857c5561436677004c8183633c5cd554d95
Zhaoyu Wei <weixiaobo88@gmail.com>
2014-04-24 20:50:44 +0800

    finish basic post and can get data

--- src/main/java/linne/AddBookServlet.java
+++ src/main/java/linne/AddBookServlet.java
@@ -1,5 +1,6 @@
 package linne;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -8,6 +9,27 @@ import java.io.IOException;
 public class AddBookServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.getWriter().println("<h1>" + "Book Shelf" + "</h1>");
+        response.getWriter().println("<form action=\"add\" method=\"POST\">\n" +
+                " ISBN: <input type=\"text\" name=\"isbn\">\n" +
+                "  name: <input type=\"text\" name=\"name\">\n" +
+                "  price: <input type=\"text\" name=\"price\">\n" +
+                "  author: <input type=\"text\" name=\"author\">\n" +
+                "  <input type=\"submit\" value=\"Submit form \"/>\n" +
+                "  </form>");
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int isbn = Integer.parseInt(request.getParameter("isbn"));
+        String name = request.getParameter("name");
+        double price = Double.parseDouble(request.getParameter("price"));
+        String author = request.getParameter("author");
+
+        System.out.println(isbn);
+        System.out.println(name);
+        System.out.println(price);
+        System.out.println(author);
 
     }
 }
